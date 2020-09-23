bf9a2ed067fd13c4485d55bb847452c9d7fb00b4
Reyad-Melies <reyad.melies@gmail.com>
2020-09-23 20:46:23 +0200

    Add project files.

---
+++ Data/EmployeeContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Employee.Models;
+namespace Employee.Data
+{
+    public class EmployeeContext:DbContext
+    {
+        public EmployeeContext(DbContextOptions<EmployeeContext> options)
+           : base(options)
+        {
+        }
+
+        public DbSet<Emp> Emp { get; set; }
+        public DbSet<Vacation> Vacation { get; set; }
+
+    }
+}
---
+++ Models/Emp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Employee.Models
+{
+    public class Emp
+    {
+        public int Id { get; set; }
+        [Required]
+        public string FullName { get; set; }
+        public string Email { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime Birthdate { get; set; }
+        [Required]
+        public string Gender { get; set; }
+        public virtual ICollection<Vacation> Vacation { get; set; }
+    }
+  
+}
+
+
