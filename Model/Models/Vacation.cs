80379f177c644383f2824c50d092ed51c4195c2b
Reyad-Melies <61639713+Reyad-Melies@users.noreply.github.com>
2020-10-03 19:48:27 +0200

    Split models to new project

---
+++ Model/Models/Vacation.cs
@@ -0,0 +1,17 @@
+﻿namespace Employee.Models
+{
+    public class Vacation
+    {
+        public int Id { get; set; }
+
+
+        public int EmpId { get; set; }
+
+        public string Type { get; set; }
+
+        public int Balance { get; set; }
+
+        public int Used { get; set; }
+        public Emp Emp { get; set; }
+    }
+}
