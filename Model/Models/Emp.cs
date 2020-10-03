35c9f2f7d95f1e12559c2748cbdac52220240e9a
Reyad-Melies <61639713+Reyad-Melies@users.noreply.github.com>
2020-10-03 19:36:53 +0200

    Add files via upload

---
+++ Model/Models/Emp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Employee.Models
+{
+    public class Emp
+    {
+        public int Id { get; set; }
+        [Required]
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime Birthdate { get; set; }
+        [Required]
+        public string Gender { get; set; }
+
+        public virtual ICollection<Vacation> Vacations { get; set; }
+    }
+
+}
+
+
